2abd076cd52035c50c8fa756fa2da3e2ee99bea4
kagemusha <mmadrid99@gmail.com>
2017-02-02 23:41:28 -0500

    seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+IO.puts "db seeding!"
+
+import Ecto.Query, only: [from: 1]
+alias Rotation.Repo
+alias Rotation.Player
+alias Rotation.Pair
+alias Rotation.Pairing
+
+Enum.each([Player, Pair, Pairing], &((from item in &1) |> Repo.delete_all))
+
+
+active_players = ["Andrew P","Jeff L","Dennis S",
+                  "Zach M","Hannah Jane B","Michael M"]
+
+Enum.each(active_players, &(Repo.insert!(%Player{name: &1, active: true})))
+
+[p1, p2 | _] = Repo.all Player
+key = "#{p1.id}:#{p2.id}"
+IO.puts key
+
+pair = Repo.insert! %Pair{key: key, active: true, player1: p1, player2: p2}
+
+#Repo.insert! %Pairing{pair: pair, period: ~D[2017-02-06]}
